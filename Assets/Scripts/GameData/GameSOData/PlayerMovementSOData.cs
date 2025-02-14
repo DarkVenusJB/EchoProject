8dc8caff385fe7347ad352c599f3fc44fa7011a4
DarkVenus <Darkvenus.job@gmail.com>
2025-02-14 21:13:29 +0300

    update so data

---
+++ Assets/Scripts/GameData/GameSOData/PlayerMovementSOData.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+
+namespace GameData.GameSOData
+{
+    [CreateAssetMenu(fileName = "PlayerMovementConfig", menuName = "Configs/Player/Player Movement Config")]
+    public class PlayerMovementSOData : ScriptableObject
+    {
+        [Header("Movement Settings")]
+        public float moveSpeed = 7f;
+        public float acceleration = 10f;
+        public float deceleration = 10f;
+
+        [Header("Jump Settings")]
+        public AnimationCurve jumpCurve;
+        public float jumpForce = 12f;
+        public float jumpDuration = 0.5f;
+        public int extraJumps = 1;
+        public float coyoteTime = 0.15f;
+        public float jumpBufferTime = 0.2f;
+        public float fallMultiplier = 2.5f;
+        public float lowJumpMultiplier = 2f;
+
+        [Header("Wall Slide Settings")]
+        public float wallSlideSpeed = 2f;
+        public float wallJumpForce = 10f;
+        public float wallJumpDirection = 1.2f;
+
+        [Header("Dash Settings")]
+        public float dashSpeed = 20f;
+        public float dashDuration = 0.2f;
+        public float dashCooldown = 1f;
+        public int airDashes = 1;
+
+        [Header("Collision Settings")]
+        public float checkRadius = 0.2f;
+    }
+}
